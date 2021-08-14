7a36dbd4c416cdd0764b82c309dd7b65609a67b2
paulocost4 <pcesarcosta@outlook.com>
2021-08-14 19:34:36 -0300

    função de subir do botão subir

--- pages/produtos/index.js
+++ pages/produtos/index.js
@@ -3,18 +3,26 @@ import Navbar from '../src/components/navbar'
 import Footer from '../src/components/footer'
 import BannerHorizontal from '../src/components/BannerPropagandaHorizontal'
 import GoogleAds from '../src/components/GoogleAds'
+import BotaoSubir from '../src/components/botaoSubir'
 
 function produtos(){
+    
     return(
         <div>
             <Navbar/>
             <BannerHorizontal/>
             {/* <GoogleAds slot='2194687109'/> */}
             <main>
+
+                                    {/* Começar a fazer a guia de produtos daqui */}
                 <h1>produtos</h1>
+
+
+                
             </main>
             <BannerHorizontal/>
             {/* <GoogleAds slot='3271965051'/> */}
+            <BotaoSubir/>
             <Footer/>
         </div>
     )
--- pages/src/components/botaoSubir/index.js
+++ pages/src/components/botaoSubir/index.js
@@ -1,22 +1,37 @@
 import styles from './Botaosubir.module.css'
 import { IoIosArrowUp } from "react-icons/io";
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 
+async function irParaOTopo(){
+  let botaoSubir = document.getElementsByClassName(styles.botaoSubir)[0]
+  
+  window.scrollTo({top: 0})
+  let aux = 1
+  while (aux){
+    if( window.scrollY===0 && botaoSubir.classList.length > 1 ){
+      console.log(`qtd class: ${botaoSubir.classList.length}, window.scrollY: ${window.scrollY}  `)
+      botaoSubir.classList.remove(styles.botaoSubirAtivo)
+      aux = 0
+    }
+  }
+}
 
 export default function BotaoSubir(props){
 
     useEffect(()=>{
-        addEventListener('scroll', (event)=>{
-          console.log('Evento disparado: ', event.type)
-          let botaoSubir = document.getElementsByClassName(styles.botaoSubir)[0]
-          botaoSubir.classList.add(styles.botaoSubirAtivo)
-        })
-        removeEventListener('scroll', null)
+        
+        document.addEventListener('scroll', (event)=>{
+          if(window.scrollY!==0){
+            console.log('Evento disparado: ', event.type)
+            let botaoSubir = document.getElementsByClassName(styles.botaoSubir)[0]
+            botaoSubir.classList.add(styles.botaoSubirAtivo)
+          }
+        }, )
       }, [])
 
     return(
-        <button className={styles.botaoSubir}>
+        <button onClick={()=>irParaOTopo()} className={styles.botaoSubir}>
             <IoIosArrowUp size='25' color='#fff' />
         </button>
     )
