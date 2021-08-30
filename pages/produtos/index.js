0ac24987c6991aca7aafa65aad41f91d7d5722b4
paulocost4 <pcesarcosta@outlook.com>
2021-08-30 08:09:06 -0300

    troca do get() pelo once() por questões de desempenho e largura de banda

--- pages/produtos/index.js
+++ pages/produtos/index.js
@@ -11,6 +11,7 @@ import {FaShoppingCart} from   'react-icons/fa'
 
 
 import { firebase, realtime, pegarImagem, firebaseConfig, storage } from '../../src/firebase'
+import { useEffect } from 'react'
 
 
 export async function getStaticProps(context) {
@@ -30,79 +31,77 @@ export async function getStaticProps(context) {
     let logoUrl = await pegarImagem( logoref )  
 
     // PRODUTOS
-    let produtos = (await realtime.ref('/produtos/').get()).toJSON()
-    
-    
-   
-      
-        
-    function parseVet(dados){   
-        let vet = []
-        for (let key in dados){
-            let value = dados[key]
-            let obj = {
-                [key] : value   // propriedade key tem origem na propriedade do objeto dados
+    let produtos
+    await realtime.ref('/produtos/').once('value', (snapshot)=>{   // pega todos os dados do arquivo produto se existir 
+        if (snapshot.exists()){
+            console.log('EXISTE!!!')
+
+            function parseVet(dados){   
+                let vet = []
+                for (let key in dados){
+                    let value = dados[key]
+                    let obj = {
+                        [key] : value   // propriedade key tem origem na propriedade do objeto dados
+                    }
+                    vet.push( obj ) // adiciona cada propriedade do objeto dados a um vetor
+                }
+                return vet // retorna um vetor de objetos
             }
-            vet.push( obj ) // adiciona cada propriedade do objeto dados a um vetor
-        }
-        return vet // retorna um vetor de objetos
-    }
 
-    let vetItens = parseVet(produtos.itens)    
-    let vetCards = parseVet(produtos.cards)
-    
-    let itens = ( ()=>{
-        let vet = []
-        vetItens.forEach( value =>{   // percorre os itens do vetor
-            let itens = value   
-            for( let key in itens){   // percorre os objetos dentro dos itens
-                let produtos = itens[key]
-                for( let produtoKey in produtos ){  // percorre os produtos
-                  let obj = {
-                    [produtoKey]: produtos[produtoKey]
-                  }  
-                  vet.push(obj)  // adiciona os produtos ao vetor
-                }   
-            } 
-        } )
-        return vet  // retorna o vetor com todos os produtos em sequencia, sem estrutura de propriedade e valor senão da propria chave de cada produto
-    } )();
+            let {cards:snapshotCard, itens:snapshotItem} = snapshot.val()
+            let vetItens = parseVet(snapshotItem)      // transforma em um vetor de objetos 
+            let vetCards = parseVet(snapshotCard)      // transforma em um vetor de objetos 
 
-    
 
-    // FIltrar aqui...
+            // Aqui eu realizo algumas transformações nos dados em PRODUTOS e ITENS para que eles fiquem da forma que eu quero utilizar 
+            let itens = ( ()=>{
+                let vet = []
+                vetItens.forEach( value =>{   // percorre os itens do vetor
+                    let itens = value   
+                    for( let key in itens){   // percorre os objetos dentro dos itens
+                        let produtos = itens[key]
+                        for( let produtoKey in produtos ){  // percorre os produtos
+                          let obj = {
+                            [produtoKey]: produtos[produtoKey]
+                          }  
+                          vet.push(obj)  // adiciona os produtos ao vetor
+                        }   
+                    } 
+                } )
+                return vet  // retorna o vetor com todos os produtos em sequencia, sem estrutura de propriedade e valor senão da propria chave de cada produto
+            } )();
 
-    
-    // cards é um objeto no formato =>   {'-MhxQlWhj8kHD4EkwcF3': { item: [Object], title: 'Bolos para encomenda' }}
-    produtos = vetCards.map( (cards, cardsIndex) => {    
-        let cardKey = Object.keys(cards)[0]
-        let cardItem = cards[cardKey].item
+            // cards é um objeto no formato =>   {'-MhxQlWhj8kHD4EkwcF3': { item: [Object], title: 'Bolos para encomenda' }}
+            produtos = vetCards.map( (cards, cardsIndex) => {    
+                let cardKey = Object.keys(cards)[0]
+                let cardItem = cards[cardKey].item
+                
+                let card = cards[cardKey] // É exatamente o objeto dentro do objeto cards, é ai que estão o item e o titulo do card
         
-        let card = cards[cardKey] // É exatamente o objeto dentro do objeto cards, é ai que estão o item e o titulo do card
+                let produtosFiltrados = []
+                itens.forEach( (produtos, itemIndex) => {    
+                    let produtoKey = Object.keys(produtos)[0]
+                    for(let key in cardItem){       // cards[key] =>  {0: MhxQlWhj8kHD4EkwcF3, 1: MhxQlWhj8kHD4EkwcF3}     
+                        if ( cardItem[key] === produtoKey ){
+                            produtosFiltrados.push(produtos)     //
+                        }
+                    }   
+                } )
+        
+                let obj = {
+                    title: card.title,  
+                    item: produtosFiltrados
+                }
+        
+                return obj  // retorna um objeto com item e o titulo do cards, os itens sao organizados da maneira abaixo
+                //vetor de vetores de objeto let cards = [ [{'id': value}], [{'id2': value2}, {'id3': value2}] ]
+            } )
 
-        let produtosFiltrados = []
-        itens.forEach( (produtos, itemIndex) => {    
-          let produtoKey = Object.keys(produtos)[0]
-          for(let key in cardItem){       // cards[key] =>  {0: MhxQlWhj8kHD4EkwcF3, 1: MhxQlWhj8kHD4EkwcF3}     
-            if ( cardItem[key] === produtoKey ){
-              produtosFiltrados.push(produtos)     //
-            }
-          }   
-        } )
-
-        let obj = {
-          item: produtosFiltrados,
-          title: card.title 
         }
+        else console.log('Dados não existentes')
+    }).catch(error => console.log('Erro ao pegar dados no bd {/produtos/}: ', error) )
 
-        return obj  // retorna um objeto com item e o titulo do cards, os itens sao organizados da maneira abaixo
-        //vetor de vetores de objeto let cards = [ [{'id': value}], [{'id2': value2}, {'id3': value2}] ]
-    } )
-    console.log(produtos)
-
-
-  
-
+    
     console.log('********* PRODUTOS *********')
 
     
@@ -156,8 +155,14 @@ function Cards({titulo, listImgs}){
     )
 }
 
-export default function produtos({produtos, logoUrl, listImgs}){
+export default function Produtos({produtos, logoUrl, listImgs}){
+    
+    useEffect(()=>{
+        console.log(produtos)
+    }, [])
+    
     /*
+      dados estruturados para a prop produtos separado por cards (indices do vetor produto)
       produtos => [
         { item: [ [Object], [Object] ], title: 'Bolos para encomenda' },
         { item: [ [Object], [Object] ], title: 'Fatias disponiveis' },
@@ -183,7 +188,7 @@ export default function produtos({produtos, logoUrl, listImgs}){
                 <section className={styles.section} >
 
                    {/* <Categorias listImgs={props.listImgs} titulo='Fatias Disponiveis' />
-                   <Categorias listImgs={props.listImgs} titulo='Bolos para encomendar' /> */}
+                   <Categorias listImgs={props.listImgs} titulo='Bolos para encomendar' />  */}
 
 
 
@@ -205,3 +210,153 @@ export default function produtos({produtos, logoUrl, listImgs}){
 // Google adsense
 // 50.000  =>  $1,920
 // 200     =>  $7.68         
+/*
+[
+    {
+        "item": [
+            {
+                "-MhxS6SS7yWaysPCATX2": {
+                    "images": {
+                        "0": "gs://dincy-s-cake.appspot.com/imagens/produtos/bolo caseirinho/caseirinho.jpg"
+                    },
+                    "price": {
+                        "grande": "",
+                        "medio": "",
+                        "pequeno": "",
+                        "unidade": "R$30,00"
+                    },
+                    "sabores": {
+                        "cobertura": {
+                            "0": ""
+                        },
+                        "massa": {
+                            "0": "Baunilha",
+                            "1": "Coco",
+                            "2": "Chocolate"
+                        }
+                    },
+                    "subtitle": "tradicional e clássico pra comer com café,",
+                    "title": "Bolo caseirinho"
+                }
+            },
+            {
+                "-MhxSyKfDFn4l40NI7ZJ": {
+                    "images": {
+                        "0": "gs://dincy-s-cake.appspot.com/imagens/produtos/bolo festeiro/20210822_115717.jpg"
+                    },
+                    "price": {
+                        "grande": "R$100,00",
+                        "medio": "R$70,00",
+                        "pequeno": "R$50,00",
+                        "unidade": ""
+                    },
+                    "sabores": {
+                        "cobertura": {
+                            "0": "chantilinho"
+                        },
+                        "massa": {
+                            "0": "cacau",
+                            "1": "baunilha",
+                            "2": "Coco",
+                            "3": "biscoito"
+                        },
+                        "recheio": {
+                            "0": "brigadeiro trufado",
+                            "1": "maracujá",
+                            "2": "Coco",
+                            "3": "Ameixa",
+                            "4": "brigadeiro de leite em pó"
+                        }
+                    },
+                    "subtitle": "Decorados e confeitados com massas de cacau, baunilha, Coco e biscoito, para recheios opções como brigadeiro trufado, maracujá, Coco, ameixa e brigadeiro de leite em pó e cobertura de chantilinho.",
+                    "title": "Bolo festeiro"
+                }
+            }
+        ],
+        "title": "Bolos para encomenda"
+    },
+    {
+        "item": [
+            {
+                "-MhxTPFXiISY1nXNhASv": {
+                    "images": {
+                        "0": "gs://dincy-s-cake.appspot.com/imagens/produtos/fatias/maracujá.jpg"
+                    },
+                    "price": {
+                        "grande": "",
+                        "medio": "",
+                        "pequeno": "",
+                        "unidade": "R$5,00"
+                    },
+                    "sabores": {
+                        "cobertura": {
+                            "0": "chantilinho"
+                        },
+                        "massa": {
+                            "0": "Biscoito"
+                        },
+                        "recheio": {
+                            "0": "Brigadeiro trufado e brigadeiro de leite em pó"
+                        }
+                    },
+                    "subtitle": "...",
+                    "title": "Bolo cookie 'n creame"
+                }
+            },
+            {
+                "-MhxTcVjYi9aIrYi1pV5": {
+                    "images": {
+                        "0": "gs://dincy-s-cake.appspot.com/imagens/produtos/fatias/maracujá.jpg"
+                    },
+                    "price": {
+                        "grande": "",
+                        "medio": "",
+                        "pequeno": "",
+                        "unidade": "R$3,50"
+                    },
+                    "sabores": {
+                        "massa": {
+                            "0": "Cacau"
+                        },
+                        "recheio": {
+                            "0": "Brigadeiro trufado e Maracujá"
+                        }
+                    },
+                    "subtitle": "...",
+                    "title": "Bolo explosão de maracujá"
+                }
+            }
+        ],
+        "title": "Fatias disponiveis"
+    },
+    {
+        "item": [
+            {
+                "-MhxTxBmxwbWS966gJOd": {
+                    "images": {
+                        "0": "gs://dincy-s-cake.appspot.com/imagens/produtos/cupcake/cupcake.jpg"
+                    },
+                    "price": {
+                        "grande": "",
+                        "medio": "",
+                        "pequeno": "",
+                        "unidade": "R$3,50"
+                    },
+                    "sabores": {
+                        "cobertura": {
+                            "0": "Chantilinho e brilho"
+                        },
+                        "massa": {
+                            "0": "Chocolate"
+                        }
+                    },
+                    "subtitle": "...",
+                    "title": "Jardim secreto"
+                }
+            }
+        ],
+        "title": "Cupcakes"
+    }
+]
+
+*/
